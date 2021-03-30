bffa34b24ad3ae917d9e8ccc409dd8acaeb50c98
danalmeidadev <devdanalmeida@gmail.com>
2021-03-30 08:58:24 -0300

    ajust service url

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import { CookieStorage } from "cookie-storage";
 
 const cookieStorage = new CookieStorage();
-/* export const baseURL = "https://api-homologa-semasa.cityconnect.com.br"; */
-export const baseURL = "http://localhost:8000";
+export const baseURL = "https://api-homologa-semasa.cityconnect.com.br";
+/* export const baseURL = "http://localhost:8000"; */
 
 const api = axios.create({
-/*   baseURL: "https://api-homologa-semasa.cityconnect.com.br/api/v1/",
-}); */
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://api-homologa-semasa.cityconnect.com.br/api/v1/",
 });
-
+/*   baseURL: "http://localhost:8000/api/v1/",
+});
+ */
 api.interceptors.request.use(function (config) {
   const token = cookieStorage.getItem('@senarsemasa:token');
   config.headers.Authorization =  token ? `Bearer ${token}` : '';
