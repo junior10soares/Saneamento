62c3d78caff987b7efddba666e2a62eb39473a2b
danalmeidadev <devdanalmeida@gmail.com>
2021-04-07 18:00:38 -0300

    base url

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { CookieStorage } from "cookie-storage";
 
 const cookieStorage = new CookieStorage();
-export const baseURL = "https://api-homologa-semasa.cityconnect.com.br";
+export const baseURL = "https://api-sanear.cityconnect.com.br";
  /* export const baseURL = "http://localhost:8000"; */
 
 const api = axios.create({
-  baseURL: "https://api-homologa-semasa.cityconnect.com.br/api/v1/",
+  baseURL: "https://api-sanear.cityconnect.com.br/api/v1/",
 });
 /*   baseURL: "http://localhost:8000/api/v1/",
 }); */
