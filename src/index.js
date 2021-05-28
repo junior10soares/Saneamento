887f9f7b00c5b0c5c2fe04180974cf4a83ee0804
Lucas Freitas <lucasdef.miranda@gmail.com>
2021-05-28 16:14:19 -0300

    adiciona GA

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GA4React, { useGA4React } from "ga-4-react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ga4react = new GA4React('G-LFF1J7BL25');
+
+(async () => {
+  await ga4react.initialize();
+  
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})();
