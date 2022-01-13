7789209141f14d5e90b018461b17f6fb93b06161
Carlos Eduardo Marcelino de Almeida <carloseduardo.ma89@gmail.com>
2022-01-13 17:31:04 -0300

    Atualização do texto e inclusão de icones

--- src/Pages/Site/Residuos/index.js
+++ src/Pages/Site/Residuos/index.js
@@ -8,17 +8,25 @@ import {
   BannerPage,
   HeaderPage,
   TextSection,
-  EstudoCatadores
+  EstudoCatadores,
 } from "./styles";
+import { DocumentsList } from "../../../components/DocumentsList";
 import MapComponent from "../../../components/MapComponent";
 import "pure-react-carousel/dist/react-carousel.es.css";
 import { Footer } from "../../../components/Footer";
 import { AlbumPhotos } from "../../../components/AlbumPhotos";
 import WorkCarousel from "../../../components/WorkCarousel";
 import api from "../../../services/api";
-import capa from '../../../assets/public/imagens/ResiduosSolidos/capa.png';
+import capa from "../../../assets/public/imagens/ResiduosSolidos/capa.png";
 
-import { FiClipboard } from 'react-icons/fi';
+import monitoramento from "../../../assets/public/imagens/ResiduosSolidos/security.png";
+import tablets from "../../../assets/public/imagens/ResiduosSolidos/tablets.png";
+import seguranca from "../../../assets/public/imagens/ResiduosSolidos/security-system.png";
+import compactadora from "../../../assets/public/imagens/ResiduosSolidos/garbage.png";
+import cabeamento from "../../../assets/public/imagens/ResiduosSolidos/cable.png";
+import comunicacao from "../../../assets/public/imagens/ResiduosSolidos/communication.png";
+
+import { FiClipboard } from "react-icons/fi";
 
 const locations = [
   {
@@ -39,17 +47,17 @@ const locations = [
   {
     name: "",
     lat: "-23.7303635",
-    lng: "-46.4945559", 
+    lng: "-46.4945559",
   },
   {
-    name: 'Jardim',
-    lat: '-23.6519862',
-    lng: '-46.5312363'
+    name: "Jardim",
+    lat: "-23.6519862",
+    lng: "-46.5312363",
   },
   {
-    name: 'Jd. Alvorada',
-    lat: '-23.6942366',
-    lng: '-46.5218557'
+    name: "Jd. Alvorada",
+    lat: "-23.6942366",
+    lng: "-46.5218557",
   },
   {
     name: "Palmeiras",
@@ -57,9 +65,9 @@ const locations = [
     lng: "-46.5403729",
   },
   {
-    name: 'Santa Cristina',
-    lat: '-23.6981998',
-    lng: '-46.5083826'
+    name: "Santa Cristina",
+    lat: "-23.6981998",
+    lng: "-46.5083826",
   },
   {
     name: "Jd. Ana Maria",
@@ -73,16 +81,20 @@ const locations = [
   },
 ];
 
-  const Residuos = () => {
+const Residuos = () => {
   const [isVisible, setVisible] = useState(false);
-  const [constructions, setConstrutions] = useState([]);  
+  const [constructions, setConstrutions] = useState([]);
   const construction = useCallback(() => {
-    api.get('work?filters[][work_category.uuid@equal]=bb7f1740-35be-11eb-8cc1-1540c95fed24').then(({ data }) => setConstrutions(data.data));
-   }, [])
+    api
+      .get(
+        "work?filters[][work_category.uuid@equal]=bb7f1740-35be-11eb-8cc1-1540c95fed24"
+      )
+      .then(({ data }) => setConstrutions(data.data));
+  }, []);
 
-   useEffect(() => {
-    construction()
-   }, [construction]);
+  useEffect(() => {
+    construction();
+  }, [construction]);
 
   const hasPictures = constructions.find(
     (construction) => construction.work_pictures.length > 0
@@ -100,7 +112,7 @@ const locations = [
       <Header />
       <BannerPage image={capa} />
       <Container>
-        <div className="desktopStepBar" style={{ width: "100%"}}>
+        <div className="desktopStepBar" style={{ width: "100%" }}>
           <div className="groupButtons">
             <div className="buttonAlign">
               <EstudoCatadores href="/estudo-de-catadores" target="_blank">
@@ -112,7 +124,7 @@ const locations = [
         </div>
 
         <Center>
-          <HeaderPage style={{marginBottom: 60}}>
+          <HeaderPage style={{ marginBottom: 60 }}>
             <TitleSection center>
               Gestão de <strong>Resíduos Sólidos</strong>
             </TitleSection>
@@ -120,7 +132,11 @@ const locations = [
           <div className="responsiveStepBar">
             <div className="groupButtonsResponsive">
               <div className="buttonAlignResponsive">
-                <EstudoCatadores style={{ minWidth: 200}} href="/estudo-de-catadores" target="_blank">
+                <EstudoCatadores
+                  style={{ minWidth: 200 }}
+                  href="/estudo-de-catadores"
+                  target="_blank"
+                >
                   <FiClipboard />
                   Estudo de catadores
                 </EstudoCatadores>
@@ -129,43 +145,85 @@ const locations = [
           </div>
           <TextSection style={{ marginTop: 110 }} columns="2">
             <p style={{ textAlign: "justify" }}>
-            Com o Programa Sanear Santo André, o Semasa terá mais <strong>20 Estações de Coleta</strong>, o que dobrará a capacidade de o município receber resíduos 
-            recicláveis, entulho, móveis, restos de pequenas construções, pneus, óleo de cozinha e lixo eletrônico (computador, geladeira, fogão etc).
+              Com o Programa Sanear Santo André, o Semasa pretende construir
+              mais 15 Estações de Coleta e implantar novos PEVS (Pontos de
+              Entrega Voluntária) para distribuir em diversos locais da cidade,
+              o que pode dobrar a capacidade de o município receber resíduos
+              recicláveis, entulho, móveis, restos de pequenas construções,
+              pneus, óleo de cozinha e lixo eletrônico (computador, geladeira,
+              fogão etc). Atualmente, o município possui 20 ecopontos.
             </p>
             <p style={{ textAlign: "justify" }}>
-            Em 2022, serão construídos mais 10 eco pontos. Os novos equipamentos têm a missão de:
+              Em 2022, começa a construção de 10 Estações de Coleta,
+              beneficiando diretamente mais de 170 mil pessoas. Os novos
+              equipamentos têm a missão de:
+            </p>
+            <p style={{ textAlign: "justify" }}>• Ampliar a reciclagem.</p>
+            <p style={{ textAlign: "justify" }}>
+              • Diminuir pontos de descarte irregular de resíduos.
             </p>
             <p style={{ textAlign: "justify" }}>
-              • Ampliar a reciclagem.
-              </p>
-            <p style={{ textAlign: "justify" }}>
-            • Diminuir pontos de descarte irregular de resíduos.
-            </p>
-            <p style={{ textAlign: "justify" }}>
-            • Aumentar a criação de postos de trabalho nas duas cooperativas
+              • Aumentar a criação de postos de trabalho nas duas cooperativas
               de reciclagem que são parceiras da cidade – a CoopCicla e a
               CidadeLimpa.
             </p>
             <p style={{ textAlign: "justify" }}>
-            • Proporcionar economia de recursos gastos em locais que sofrem com o despejo de materiais.
+              • Proporcionar economia de recursos que são utilizados para a
+              limpeza de pontos de descarte irregular de resíduos
             </p>
             <p style={{ textAlign: "justify" }}>
-            <strong>Mais de 170 mil pessoas vão ser beneficiadas</strong> com os novos ecopontos. Ao todo, a cidade passará a ter 40 equipamentos públicos para 
-            recebimento de resíduos. O raio de distância entre as unidades será de até 1,5 quilômetro.
+              As novas Estações de Coleta serão mais <strong>modernas</strong>.
+              As unidades vão receber caçambas compactadoras para melhorar a
+              eficiência do sistema e reduzir o número de viagens de caminhões
+              que levam os resíduos às cooperativas de reciclagem. Além disso,
+              os funcionários que trabalham nos ecopontos vão ganhar tablets
+              para fazer controle das pessoas que acessam as Estações de Coleta.
+              Atualmente, o serviço é feito a mão.
             </p>
             <p style={{ textAlign: "justify" }}>
-            As novas Estações de Coleta serão mais <strong>modernas</strong>. Os locais vão receber caçambas compactadoras para melhorar a eficiência do sistema 
-              e reduzir o número de viagens de caminhões que levam os resíduos às cooperativas de reciclagem.          
-            </p>
-            <p style={{ textAlign: "justify" }}>
-            Todos os equipamentos públicos também terão um sistema de filmagem de qualidade para que possam ser evitados furtos e invasões. 
-            Para isso, haverá monitoramento dos locais em um Centro de Controle Operacional. Veja abaixo a <strong>localização dos 10 ecopontos</strong> que 
-            serão construídos no começo de 2022.
+              Todos os equipamentos públicos também terão um sistema de filmagem
+              de qualidade para que possam ser evitados furtos e invasões. Para
+              isso, haverá monitoramento dos locais em um Centro de Controle
+              Operacional, espaço que ainda será construído.
             </p>
           </TextSection>
         </Center>
-      </Container>
+        <p style={{fontWeight: "bold"}}>Infraestrutura das novas estações de coleta:</p>
+        <DocumentsList>
+                <div className="Grid">
+                  <div className="row">
+                  <span className="span-column" style={{marginLeft: 150, marginRight: 150}}>
+                      <img style={{ height: 50, width: 50}} src={monitoramento} />
+                      <p style={{margin: 0, flexWrap: 'wrap'}}>Câmeras de monitoramento</p>
+                    </span>
+                    <span className="span-column" style={{marginLeft: 80, marginRight: 150}}>
+                      <img style={{ height: 50, width: 50}} src={tablets} />
+                      <p style={{margin: 0, flexWrap: 'wrap'}}>Tablets</p>
+                    </span>
+                    <span className="span-column" style={{marginLeft: 50, marginRight: 50}}>
+                      <img style={{ height: 50, width: 50}} src={seguranca} />
+                      <p style={{margin: 0, marginRight:0, flexWrap: 'wrap'}}>Sistema de Segurança</p>
+                    </span>
+                  </div>
 
+                  <div className="row">
+                    <span className="span-column" style={{marginLeft: 160, marginRight: 150}}>
+                      <img style={{ height: 50, width: 50}} src={cabeamento} />
+                      <p style={{margin: 0}}>Cabeamento Estruturado</p>
+                    </span>
+                    <span className="span-column" style={{marginLeft: 20, marginRight: 150}}>
+                      <img style={{ height: 50, width: 50}} src={compactadora} />
+                      <p style={{margin: 0}}>Caçambas Compactadoras</p>
+                    </span>
+                    <span className="span-column" style={{marginLeft: -35, marginRight: 0}}>
+                      <img style={{ height: 50, width: 50}} src={comunicacao} />
+                      <p style={{margin: 0}}>Sistemas de comunicação</p>
+                    </span>
+                  </div>
+                </div>
+              </DocumentsList>
+      </Container>
+                
       {hasPictures && hasVideos && (
         <Container>
           <Center>
@@ -186,7 +244,10 @@ const locations = [
             </Row>
           </Center>
         )}
-
+         <TextSection style={{ marginTop: 110 }} columns="2">
+            <p style={{ textAlign: "justify" }}>
+          Veja abaixo a <strong>localização dos novos 10 ecopontos.</strong> </p>
+          </TextSection>
         <Center>
           <h2>Ecopontos</h2>
           <MapComponent markers={locations} />
@@ -195,6 +256,6 @@ const locations = [
       <Footer />
     </>
   );
-}
+};
 
 export default Residuos;
