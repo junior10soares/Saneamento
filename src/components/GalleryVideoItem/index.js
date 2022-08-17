65ddbfc59991ac2a3d85488b5d4331ba33f6a069
CityConnect <desenvolvimento@cityconnect.com.br>
2022-08-17 12:11:12 -0300

    alterações para deletar os videos

--- src/components/GalleryVideoItem/index.js
+++ src/components/GalleryVideoItem/index.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Card, Image } from "./style";
+import { Card, Image, Button } from "./style";
+import { FaTrash } from 'react-icons/fa';
 import ReactPlayer from "react-player/lazy";
 
-export function GalleryVideoItem({ video }) {
+export function GalleryVideoItem({ video, handleDeleteVideo }) {
   return (
     <Card>
       <ReactPlayer
         url={video.url}
         width='100%'
         height='100%'
-        controls
+        // controls
       />
+      <Button onClick={()=>{if(confirm(`Tem certeza que deseja apagar o vídeo "${video.name}" ?`)){handleDeleteVideo(video.uuid)}}}><FaTrash /></Button>
     </Card>
+    
   );
 }
