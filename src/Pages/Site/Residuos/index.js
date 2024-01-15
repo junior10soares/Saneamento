372f42f781c470741da94116e496b957fe34a6dc
Carlos Eduardo Marcelino de Almeida <carloseduardo.ma89@gmail.com>
2024-01-15 13:53:42 -0300

    Alterações pagina 'resíduos sólidos' (SEM-69)

--- src/Pages/Site/Residuos/index.js
+++ src/Pages/Site/Residuos/index.js
@@ -31,6 +31,17 @@ import ProjetoSocioAmbiental from "../../../assets/public/imagens/ResiduosSolido
 import ProjetoSocioAmbiental2 from "../../../assets/public/imagens/ResiduosSolidos/socioambiental2.png";
 import LivrosobreCatadores from "../../../assets/public/imagens/ResiduosSolidos/livrocatadores.png";
 import EstudosobreCatadores from "../../../assets/public/imagens/ResiduosSolidos/estudocatadores.png";
+// imagens
+import bangu from "../../../assets/public/imagens/ResiduosSolidos/EC-Bangu.jpg";
+import palmeiras from "../../../assets/public/imagens/ResiduosSolidos/EC-Palmeiras.jpg";
+import centro from "../../../assets/public/imagens/ResiduosSolidos/EC-Centro.jpg";
+import jardimAlvorada from "../../../assets/public/imagens/ResiduosSolidos/EC-Jardim-Alvorada.jpg";
+import jardimCristiane from "../../../assets/public/imagens/ResiduosSolidos/EC-Jardim-Cristiane.jpg";
+import miami from "../../../assets/public/imagens/ResiduosSolidos/EC-Parque-Miami.jpg";
+import santoAndre from "../../../assets/public/imagens/ResiduosSolidos/EC-Jardim-Santo-Andre.jpg";
+import guiomar from "../../../assets/public/imagens/ResiduosSolidos/EC-Vila-Guiomar.jpg";
+import vilaLinda from "../../../assets/public/imagens/ResiduosSolidos/EC-Vila-Linda.jpg";
+import avCandido from "../../../assets/public/imagens/ResiduosSolidos/av-candido-camargo.jpg";
 
 import { FiClipboard } from "react-icons/fi";
 
@@ -86,7 +97,88 @@ const locations = [
     lat: "-23.690163337206005",
     lng: "-46.532107500118336",
   },
+  {
+    name: "Av. Ford, 37 - Vila de Paranapiacaba",
+    lat: "-23.776181459073563",
+    lng: "-46.298477476392144",
+  },
+  {
+    name: "Rua Edgard Vieira de Lima, 51, Parque Marajoara",
+    lat: "-23.66477457264354",
+    lng: "-46.488364045710256",
+  },
 ];
+
+const gallery2 = [
+  { url: "https://via.placeholder.com/300", caption: "Imagem 1" },
+  { url: "https://via.placeholder.com/300", caption: "Imagem 2" },
+];
+const images = [
+  {
+    url: bangu,
+    caption: "EC Bangu",
+  },
+  {
+    url: palmeiras,
+    caption: "EC Palmeiras",
+  },
+  {
+    url: centro,
+    caption: "EC Centro",
+  },
+  {
+    url: jardimAlvorada,
+    caption: "EC Jardim Alvorada",
+  },
+  {
+    url: jardimCristiane,
+    caption: "EC Jardim Cristiane",
+  },
+  {
+    url: miami,
+    caption: "EC Parque Miami",
+  },
+  {
+    url: santoAndre,
+    caption: "EC Jardim Santo André ",
+  },
+  {
+    url: guiomar,
+    caption: "EC Vila Guiomar",
+  },
+  {
+    url: vilaLinda,
+    caption: "EC Vila Linda",
+  },
+];
+
+const columnStyle = {
+  flex: "1",
+  padding: "10px",
+};
+
+const imgStyle = {
+  width: "612px",
+  height: "415px",
+  marginTop: "10px",
+};
+const imgStyle2 = {
+  width: "100%",
+  height: "auto",
+  marginTop: "10px",
+};
+
+const captionStyle = {
+  textAlign: "center",
+  fontSize: "14px",
+  color: "#555",
+  fontStyle: "italic",
+};
+const centerImageStyle = {
+  textAlign: "center",
+  marginTop: "20px", 
+  marginBottom:"30px",
+};
 
 const Residuos = () => {
   const [isVisible, setVisible] = useState(false);
@@ -114,8 +206,6 @@ const Residuos = () => {
   const hasVideos = constructions.find(
     (construction) => construction.work_videos.length > 0
   );
-
-  
 
   return (
     <>
@@ -218,83 +308,135 @@ const Residuos = () => {
             id="firstParagraph"
           >
             <p style={{ textAlign: "justify" }}>
-              Com o Programa Sanear Santo André, a Prefeitura e o Semasa vão
-              construir mais 10 Estações de Coleta e implantar novos PEVS
-              (Pontos de Entrega Voluntária) para receber resíduos recicláveis,
-              eletroeletrônicos e outros materiais, o que vai beneficiar
-              diretamente mais de 170 mil pessoas.
-              <strong> As intervenções têm como objetivos </strong>:
+              Com o programa Sanear Santo André, a Prefeitura e o Semasa
+              construíram <strong>10 Estações de Coleta</strong> e{" "}
+              <strong>reformaram 2 ecopontos</strong>, beneficiando mais de 170
+              mil pessoas. Esses equipamentos públicos visam:
             </p>
             <p style={{ textAlign: "justify" }}>• Aumentar a reciclagem.</p>
             <p style={{ textAlign: "justify" }}>
-              • Reduzir o volume de materiais secos que é aterrado e ampliar a
-              vida útil do Aterro Sanitário Municipal. A previsão é de que o
-              equipamento tenha apenas mais 5 anos de operação.
+              • Reduzir o volume de materiais que é destinado ao Aterro
+              Sanitário Municipal, ampliando o tempo de operação do equipamento.
             </p>
             <p style={{ textAlign: "justify" }}>
               • Diminuir pontos de descarte irregular de resíduos.
             </p>
             <p style={{ textAlign: "justify" }}>
-              • Proporcionar economia de recursos públicos que são utilizados
-              com serviços de limpeza.
+              • Minimizar a emissão de gases do efeito estufa, como metano e
+              dióxido de carbono, que são responsáveis pelas mudanças
+              climáticas.
             </p>
             <p style={{ textAlign: "justify" }}>
-              • Deixar os bairros mais limpos e proteger o meio ambiente.
+              • Proporcionar economia de recursos públicos gastos com serviços
+              de limpeza.
             </p>
             <p style={{ textAlign: "justify" }}>
-              • Aumentar a criação de postos de trabalho nas duas cooperativas
-              de reciclagem que existem– a Coopcicla e a Cidade Limpa.
+              • Deixar os bairros mais limpos.
+            </p>
+            <p style={{ textAlign: "justify" }}>• Preservar o meio ambiente.</p>
+            <p style={{ textAlign: "justify" }}>
+              • Ampliar postos de trabalho nas cooperativas de reciclagem de
+              Santo André – Coopcicla e Cidade Limpa.
             </p>
             <p style={{ textAlign: "justify" }}>
-              Em abril de 2022, começou a construção das
-              <strong> Estações de Coleta Ana Maria, Palmeiras e Bangu </strong>
-              (confira abaixo a localização dos ecopontos).Só na região dessas
-              três unidades, a equipe de varrição do Semasa gastou, em todo o
-              ano de 2021, mais de R$ 141 mil para limpar pontos de descarte
-              irregular de resíduos. Foram retiradas 680 toneladas de materiais.
+              As obras iniciaram em abril de 2022, com a construção das Estações
+              de Coleta Ana Maria e Bangu, além da reforma da EC Palmeiras. Só
+              na avenida Cândido Camargo, no Jardim Ana Maria, onde está
+              localizado um dos ecopontos, o Semasa economiza mais de R$ 140 mil
+              por ano. Esse foi o valor gasto pela autarquia, em 2021, para
+              retirar 680 toneladas de materiais que foram descartadas
+              irregularmente no terreno desse equipamento.
+            </p>
+            {/* <div style={{ display: "flex" }}>
+              <div style={columnStyle}>
+                {gallery2.slice(0, 1).map((image, index) => (
+                  <div key={index}>
+                    <img src={image.url} alt={image.caption} style={imgStyle} />
+                    <p style={captionStyle}>{image.caption}</p>
+                  </div>
+                ))}
+              </div>
+              <div style={columnStyle}>
+                {gallery2.slice(1, 2).map((image, index) => (
+                  <div key={index + 1}>
+                    <img src={image.url} alt={image.caption} style={imgStyle} />
+                    <p style={captionStyle}>{image.caption}</p>
+                  </div>
+                ))}
+              </div>
+            </div> */}
+            <div style={centerImageStyle}>
+              <img src={avCandido} alt="Imagem Centralizada" style={imgStyle2} />
+              <p style={captionStyle}>Antes x depois da avenida Cândigo Camargo, no Jardim Ana Maria</p>
+            </div>
+            <p style={{ textAlign: "justify" }}>
+              O segundo bloco de obras contemplou a construção das Estações de
+              Coleta Centro, Jardim Alvorada e Jardim Cristiane. Já o terceiro
+              envolveu as unidades Parque Miami, Jardim Santo André, Vila
+              Guiomar e Vila Linda.
             </p>
             <p style={{ textAlign: "justify" }}>
-              O segundo bloco de obras contempla a construção das
-              <strong> Estações de Coleta Centro, Jardim Alvorada </strong> e
-              <strong> Jardim Cristiane </strong>. Já o terceiro e último bloco
-              envolve as unidades
-              <strong> Parque Miami, Jardim Santo André </strong> e
-              <strong> Vila Guiomar </strong>e<strong> Vila Linda. </strong>
-              Contando com as 10 Estações de Coleta que vão ser implantadas, o
-              Semasa pode economizar mais de R$ 500 mil por ano, caso consiga
-              combater pontos de descarte irregular. O investimento para
-              construir as novas unidades é de mais de R$ 6 milhões.
+              O quarto e último bloco de obras teve a construção da EC Parque
+              Marajoara e a reforma da EC Paranapiacaba.
             </p>
+            <div style={{ display: "flex" }}>
+              <div style={columnStyle}>
+                {images.slice(0, 3).map((image, index) => (
+                  <div key={index}>
+                    <img src={image.url} alt={image.caption} style={imgStyle} />
+                    <p style={captionStyle}>{image.caption}</p>
+                  </div>
+                ))}
+              </div>
+              <div style={columnStyle}>
+                {images.slice(3, 6).map((image, index) => (
+                  <div key={index + 3}>
+                    <img src={image.url} alt={image.caption} style={imgStyle} />
+                    <p style={captionStyle}>{image.caption}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
             <p style={{ textAlign: "justify" }}>
               <strong> Modernização dos equipamentos </strong>
             </p>
             <p style={{ textAlign: "justify" }}>
-              As novas Estações de Coleta serão mais  modernas e vão receber
-              caçambas compactadoras para melhorar a eficiência do sistema, além
-              de reduzir o número de viagens de caminhões que levam os resíduos
-              às cooperativas de reciclagem. Todos os equipamentos ainda terão
-              um sistema de filmagem de qualidade para monitorar os locais e
-              evitar a ocorrência de depredações, furtos e invasões.
+              As novas Estações de Coleta têm{" "}
+              <strong>estruturas mais modernas</strong> para melhorar o serviço
+              e atendimento aos usuários. Elas possuem caçambas compactadoras, o
+              que possibilita armazenar mais do que o dobro de resíduos que
+              caberiam em uma caçamba estacionária. Todos os ecopontos também
+              têm jardins e 2 câmeras de monitoramento para coibir furtos,
+              invasões e descarte irregular de resíduos.
             </p>
             <p style={{ textAlign: "justify" }}>
               <strong> Descarte correto nas Estações de Coleta </strong>
             </p>
             <p style={{ textAlign: "justify" }}>
-              Atualmente, Santo André disponibiliza 20 ecopontos, que são
-              equipamentos gratuitos e exclusivos para o uso dos moradores da
-              cidade. A população pode descartar recicláveis volumosos, entulho,
-              móveis, colchões e estofados, quatro pneus (por mês), óleo de
-              cozinha e resíduos eletroeletrônicos, como computador, monitor,
-              geladeira, fogão e etc. Cada pessoa pode fazer a disposição de até
-              um metro cúbico de resíduos, que é o equivalente a dez sacos de
-              lixo de 100 litros. Algumas unidades também aceitam podas de
-              vegetação, telhas de amianto e gesso. O endereço de todas as
-              Estações de Coleta, bem como os dias e horários de funcionamento,
-              pode ser consultados no{" "}
-              <a href="http://www.semasa.sp.gov.br/residuos/coleta-domiciliar-2/coleta-seletiva/estacoes-de-coleta/">
-                site do Semasa
-              </a>
-              .
+              Santo André possui <strong>30 Estações de Coleta</strong> para que
+              os munícipes possam dar a destinação ambientalmente correta a
+              diversos tipos de resíduos volumosos.
+            </p>
+            <p style={{ textAlign: "justify" }}>
+              Os locais aceitam entulho, móveis, colchões e estofados, pneus (4
+              por mês), eletroeletrônicos, óleo de cozinha usado (a cada 1
+              litro, troque por 1 barra de sabão ecológico), recicláveis,
+              isopor, dentre outros. Em algumas unidades a população ainda pode
+              levar poda de vegetação, gesso e telha e fibra de amianto.
+            </p>
+            <p style={{ textAlign: "justify" }}>
+              Também é possível doar roupas, calçados, acessórios e objetos –
+              inclusive para pessoas com deficiência. Esses itens são
+              distribuídos em projetos sociais do Semasa e doados a instituições
+              sociais e ao Fundo Social de Solidariedade, da Prefeitura de Santo
+              André.
+            </p>
+            <p style={{ textAlign: "justify" }}>
+              Cada morador pode fazer, por mês, a disposição de até 1 m³ de
+              resíduos, que é o equivalente a 10 sacos de lixo de 100 litros. O
+              endereço de todas as Estações de Coleta, bem como os dias e
+              horários de funcionamento, pode ser consultado em{" "}
+              <a href="http://www.semasa.sp.gov.br">www.semasa.sp.gov.br</a>.
             </p>
             <p
               style={{
